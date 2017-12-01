c1c50daf60e0044d3d2ac4747c6466b6773a6cd2
RaduMilici <Radu.Milici.3pg@phishme.com>
2017-12-01 17:17:21 +0200

    AppBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import RaisedButton from 'material-ui/RaisedButton';
-import AppBar from 'material-ui/AppBar';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/AppBar/AppBar';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <div>
-          <AppBar title='Near Earth Objects'/>
-          <RaisedButton label='default'/>
-        </div>
-      </MuiThemeProvider>
+        <AppBar/>
     );
   }
 }
---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AppBar = () => {
+  return <p>AppBar</p>
+}
+
+export default AppBar
