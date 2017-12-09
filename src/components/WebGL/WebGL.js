aff2106c4acc6fdb1cc36eb1f65f2265b3d006f5
Voina Andreea <voinaandreea@outlook.com>
2017-12-09 19:55:54 +0200

    scene initialization

--- src/components/WebGL/WebGL.js
+++ src/components/WebGL/WebGL.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import classes from './WebGL.scss';
-
+import { start } from '../../3d/init3d';
+import { animate, addCube } from '../../3d/updater';
 class WebGL extends Component {
   componentDidMount() {
+    start();
+    addCube();
+    animate();
     console.log('WebGL container mounted');
   }
 
   render() {
     return (
-        <div className={classes.container}></div>
+        <div id='webgl' className={classes.container}></div>
     );
   }
 }
